b4f3f632b4626da93e7a3d46223813077b851d0a
Gfelipe07 <KINDGABSLIPS@OUTLOOK.COM>
2024-11-26 15:29:59 -0300

    adição da função salvar

--- app/src/main/java/com/example/rgm32715340/ui/item/ItemEntryViewModel.kt
+++ app/src/main/java/com/example/rgm32715340/ui/item/ItemEntryViewModel.kt
@@ -1,27 +1,13 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.rgm32715340.ui.item
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.rgm32715340.data.Item
 import com.example.rgm32715340.data.ItemsRepository
+import kotlinx.coroutines.launch
 import java.text.NumberFormat
 
 /**
@@ -47,9 +33,11 @@ class ItemEntryViewModel(private val itemsRepository: ItemsRepository) : ViewMod
     /**
      * Inserts an [Item] in the Room database
      */
-    suspend fun saveItem() {
+    fun saveItem() {
         if (validateInput()) {
-            itemsRepository.insertItem(itemUiState.itemDetails.toItem())
+            viewModelScope.launch {
+                itemsRepository.insertItem(itemUiState.itemDetails.toItem())
+            }
         }
     }
 
